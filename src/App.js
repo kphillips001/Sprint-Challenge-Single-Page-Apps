72e068e9a00f4a6639b8b8de59ff59ba09547607
Kevin <knphillips01@gmail.com>
2020-01-31 11:06:06 -0600

    created links and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
+import {Route, Link, Switch} from 'react-router-dom';
 
 export default function App() {
   return (
     <main>
       <Header />
+
+      <Link to='/'>Home</Link> 
+      <Link to='/characters'>Characters</Link> 
+      <Link to='/search'>Search</Link> 
+
+      <Switch>
+        <Route exact path="/search">
+        
+        </Route>
+
+        <Route exact path="/characters">
+        <CharacterList/>
+        </Route>
+
+        <Route exact path="/">
+          <WelcomePage/>
+        </Route>
+      
+      </Switch>
+
     </main>
   );
 }
